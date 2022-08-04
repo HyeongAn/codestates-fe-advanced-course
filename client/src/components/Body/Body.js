619c64146f1100fa8760539e5d7191f3dc0cf19e
HyeongAn <guddks84@gamil.com>
2022-08-04 17:49:20 +0900

    Fix bug

--- client/src/components/Body/Body.js
+++ client/src/components/Body/Body.js
@@ -1,14 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Main from '../../pages/MainPage/MainPage'
 import Detail from '../../pages/PostDetail/PostDetail'
 
 function Body (){
     return (
         <div>
-            <Routes>
+            <HashRouter>
                 <Route path="/" element={<Main/>}/>
                 <Route path="/detail" element={<Detail/>}/>
-            </Routes>
+            </HashRouter>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Scrolltop from './ScrollTop.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Scrolltop/>
       <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
